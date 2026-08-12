3282c75595fd2b41c5feea03ce07137b88dd0b0e
Krishnendu Ghosh <24krishnendu@gmail.com>
2026-08-12 21:34:39 +0530

    Remove unnecessary blank line in JavaLoopsII.java

---
+++ hacker-rank/loops/JavaLoopsII.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int queries = sc.nextInt();
+
+        for (int i = 0; i < queries; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+
+            int term = a;
+            for (int j = 0; j < n; j++) {
+                term += (int) (Math.pow(2, j) * b);
+                System.out.print(term + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
